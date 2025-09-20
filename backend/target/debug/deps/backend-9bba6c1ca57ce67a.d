a2a3ffee025fffd21ec0da1de71c39f414d554a1
vynious <shawnthiah@gmail.com>
2025-09-20 08:51:23 +0200

    init frontend and backend

---
+++ backend/target/debug/deps/backend-9bba6c1ca57ce67a.d
@@ -0,0 +1,5 @@
+/Users/shawntyw/projects/finOS/backend/target/debug/deps/libbackend-9bba6c1ca57ce67a.rmeta: src/main.rs
+
+/Users/shawntyw/projects/finOS/backend/target/debug/deps/backend-9bba6c1ca57ce67a.d: src/main.rs
+
+src/main.rs:
---
+++ backend/target/debug/deps/backend-f3612a36dab0606f.d
@@ -0,0 +1,5 @@
+/Users/shawntyw/projects/finOS/backend/target/debug/deps/libbackend-f3612a36dab0606f.rmeta: src/main.rs
+
+/Users/shawntyw/projects/finOS/backend/target/debug/deps/backend-f3612a36dab0606f.d: src/main.rs
+
+src/main.rs:
